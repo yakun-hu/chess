61e623f15ed9eeea29eba2c6c478e352ff8a315a
Yakun Hu <116873082+yakun-hu@users.noreply.github.com>
2023-07-09 00:36:09 -0400

    Add files via upload

---
+++ testing-progress/analy.2-vert,test[A].php
@@ -0,0 +1,39 @@
+E1: Input | 1-s: S: K-d8 R[e4,]P[d7,] | F: K-f7 R[c1,][D]<br><br><?php require'innate.M-trait,class.inc-test.php';?>
+<!--first array--><?php $POSTED_assoc_f = array("f7"=>"K","c1"=>"R"/*locs from analy-1*/);$POSTED_assoc_s = array("d7"=>"P","d8"=>"K","e4"=>"R")?><!--1) square.key-attr[zi-assembl,upd]-->
+		<!--gen'd in analy-1--><?php /*catch-post*/?>
+	<!--obj-instansh--><?php /*d7-P, d8-K*/
+	$P_王 = new bing("d7","s");$P_王->__ray_cntr(array("d8"));print_r($P_王);// $P_王->function(); array index ref in var.name-2nd 
+	$K_嫁 = new 帅("f7","f");
+	$K_界 = new 帅("d8","s");
+	$R_使 = new Rook("e4","s");
+	$R_死 = new Rook("c1","f");?><!--set-fx-->
+
+<!--2nd array[f.r-r,r]--><?php $filer = array("f"=>7,"c"=>1,"d"=>array(7,8),"e"=>4);?>
+<!--3-lie[r.f-f,f]--><?php $ranker = array(1=>"c",4=>"e",7=>array("d","f"),8=>"d");?><!--emante.thru-thes>
+
+<!--4th--><?php // $E_Assoc = array($P_王, $K_嫁);//print_r($E_Assoc);?><!--only-E-->
+<!--Write a #main-func to convert E_assoc into output--><?php function E_assoc_分析2($E_Assoc){}?>
+<br><br>E2: <b>Output</b> | Output-cheq<br>
+<table><tr><td>d7-P<span class="prot-E"><1:0></span></td><td>f7-K</td></tr>
+<tr><td>d8-K<span class="neutr"><0></span></td><td>c1-R</td></tr>
+<tr><td>e4-R</td><td></td></tr></table>
+<?php // E_assoc_分析2($E_Assoc);
+$R_使->fill_scrines();//print_r($R_使);?>
+
+<!-- http://chess/testing-progress/analy.2-vert,test[A].php#h3 -->
+<!-- Notes: 
+[Late.7th-8]Problem: Statics !expr: rel | Verbal<MIC>The function that emits output, E_assoc_分析2, must echo 1) all zi locs, 2) cat'd by side, 3) E-tags any appl'b, 4) cat'd by side; 
+	It takes as an arg $E_Assoc, which contains an array of obj | These data # are curr-sctr'd, but cnt'd in few, and Rels-un,crt The original idea was<pinp>to loop through lin'd-r<r>ays and use The
+	zi.M-18	to identif zi #next
+===
+	interessant:function.array-column.php, extracts 2-d~ array from >2d array<deriv> | obj-proc; array-chunk<infr>does revers
+		2nd argu: the key-name of the return-clmn | 3rd arg: "order by"
+	https://youtu.be/PJvG9wDkrbU PHP multi.dimens-array, "array of indexed/assoc_arrays" 
+	Closer to output funcs in analyz will live in the #mains 
+	A slightly circuitous route<自然和谐>offsets future-discount
+	Ref: https://www.php.net/manual/en/language.oop5.decon.php 
+	a *de"str meth runs when obj-del think.the-un,ble array($P_王, array($K_嫁))
+	Trait use has sig-issu🔴 | Must: test.this-sep | When a class uses a trait, it cannot be called post-scr # -->
+
+<style>.prot-E{background-color:00FF00}.neutr{background-color:607399;color:white}.eE{background-color:red}</style>
+<script>document.getElementsByTagName("TITLE")[0].text="analy.2-vert,test[A].php"</script>
---
+++ testing-progress/innate.M-trait,class.inc-test.php
@@ -0,0 +1,39 @@
+<?php /*line.🔎-ops.php*/function faggeet(){echo "bridge";}
+
+/*路_nobles,traits*/
+function 并行查找_op($line_名/*RorF🌭*/, $lim=16){global $ranker;global $filer;
+//
+if(gettype($line_名)=="integer")
+{foreach($ranker as$普通=>$档案_s){/*echo $line_名;echo $key;*/if($普通==$line_名){echo "<br>count=1";/*~all-execs;loop-2;90%func-lim;public$E_array of the E'd(differ-cnt, 0.5/1)*/}}}
+else{foreach($filer as$档案=>$普通_s){if($档案==$line_名){echo "<br>mount=1";/*~all-execs*/}}}}/*R*/
+//
+function 并行查找_nop($diag_名/*🐶*/, $lim=16){}/*K*/
+/*^ini^*/
+
+/*innate.M-inc.php*/
+trait Zi {function text(){echo "Hello<br>";}public$loc;public$side;public$E_array=array();function __construct($square,$side){$this->loc=array($square[0],$square[1]);settype($this->loc[1],"int");$this->side=$side;}function __ray_cntr($array){foreach($array as $sq){array_push($this->E_array, $sq);}}} 
+
+class bing {use Zi;} class 帅 {use Zi;} class Rook {use Zi;function inc_function(){faggeet();}function fill_scrines(){并行查找_op($this->loc[0]);并行查找_op($this->loc[1]);}} 
+
+/*test.php*/
+$rook = new Rook("d8","f");
+$rook->inc_function();/*✅*/ // $rook->text(); 
+// echo gettype(3);$type = 3;echo gettype($type);/*int*/$type_2="3";echo gettype($type_2);/*str*/
+// print_r($rook);?>
+
+<!-- http://chess/testing-progress/innate.M-trait,class.inc-test.php
+"Yahoo" Babh https://www.youtube.com/watch?v=OJ5fmrMMcDU #Gi -->
+
+<?php // base-test | trait Ren{}class Bank{use Ren;}$bank=new Bank;print_r($bank);?>
+
+<!-- notes: 
+	The M-rule needs to s-eman from that$zi-loc and map the blast-radi, thru the 2 rail-rays, and finally(3) search for class-locs which match # 
+	analy next: instansh 🤨 $POSTED_assoc and 2nd/3rd Where 2,3 can be easil-deriv from $POSTED Should I spl posted into f and s-assoc? 
+	Should function 并行查找_op($line_名/*RorF🌭*/, $lim){} be env'd trait "road noble"? 
+	The issue with func.nu-wrap vs trait-use is that a trat'ud can also use $this-var
+	May: need a temp-array of Existent-objs # 
+	Associating obj-ins w/ loc-out, need this bi-dir 1) in loop to inst, and 2) search by loc to plug $E_array | there is a missing min.loa-2,d.t
+		Sol, loop-thru $POSTED_a's and repl the val, zi-type, w/ $class-var, which contains zi-type, and can be read w/ matcher # ✅ 
+			This repl # will occur on analy-2, during obj.instansh-loop -->
+
+<title>innate.M-trait,class.inc-test.php</title>
