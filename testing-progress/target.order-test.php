7a7fd4ec824aa4bcc553f3ef50d64e0ef7876874
Yakun Hu <116873082+yakun-hu@users.noreply.github.com>
2023-07-19 22:36:15 -0400

    Add files via upload
    signf prog, but my WAMP was compr

---
+++ testing-progress/target.order-test.php
@@ -0,0 +1,38 @@
+<?php require'innate.M-trait,class.inc-test.php';session_start();
+$R_出=array("d8"=>"f","d6"=>"s","e4"=>"s","h4"=>"s");$Rer="d4";$this_side="s";
+$E_出=array("f6"=>"f","d6"=>"s","b8"=>"s","c3"=>"f");$观点="e5";$side_this="f";
+function Proximity_Ju($R_出,$Rer){global$分区;$S=array();$N=array();$W=array();$E=array();$方向阵列=array($N,$W,$S,$E);
+	foreach($R_出 as $出_sq=>$side)
+	{if($Rer[0]==/*same-fl*/$出_sq[0]){if($Rer[1]>/*N-of*/$出_sq[1]){echo"~✅S:".$出_sq;}else{echo"✅N:".$出_sq;}}
+	else{if(strcmp($Rer[0],$出_sq[0])==-1){echo"✅E:".$出_sq;}else{echo"✅W:".$出_sq;}}}
+	foreach($方向阵列 as $dir){/*print-ln, refn indx-0 of the $dir-array*/}}
+function Proximity_X($E_出,$观点){global$分区;$NW=array();$NE=array();$SE=array();$SW=array();$方向阵列=array($NW,$NE,$SE,$SW);
+	foreach($E_出 as $出_sq=>$side)
+	{if((array_search($出_sq[0],$分区)+$出_sq[1])==/*same-↘️*/(array_search($观点[0],$分区)+$观点[1])){if($观点[1]>/*N-of*/$出_sq[1]){echo"✅SE:".$出_sq;}else{echo"✅NW:".$出_sq;}}
+	else{if($观点[1]>/*N-of*/$出_sq[1]){echo"✅SW:".$出_sq;}else{echo"✅NE:".$出_sq;}}}
+	foreach($方向阵列 as $dir){/*print-ln, refn indx-0 of the $dir-array*/}}/*all 4: compr by rank # in each dir, we sort the top by the opp-dir */
+/*both xng-Ju will proc 1-4 arrys array_search($路_名[0],$分区); 
+	I need to defn 8 arrays them-c<hard-c> and ref these, each, in prnt<hard.c-arry> [1]> is Eastr */
+	Proximity_Ju($R_出,$Rer);echo"<br>";Proximity_X($E_出,$观点);
+
+echo"<br>";echo strcmp("a","b");echo strcmp("b","a");?>
+
+<!-- http://chess/testing-progress/target.order-test.php -->
+<!-- Notes 
+[Late.7th-19] // add echo-mrkr With cond-sorted<fbno>by inequal-parti,alr # I simply need sort(asc) for the < group and sort(desc) for the > group; subseq, indx[0] of each gets 1 full, foreach rest, 0.5 | left or right depn $side # Outpu-lu nex
+	Side for $Per is non-spec'd<hard-c>d4-R is not in $观点-rng
+	The context that these are called is from within the class<~>after $E_Chu is assembl'd 
+[Ini]defn inpu/out "end-points":  tmrw: Sol, loop-thru $POSTED_a's and repl the val, zi-type, w/ $class-var, which contains zi-type, and can be read w/ matcher # ✅ 
+This repl # will occur on analy-2, during obj.instansh-loop Next: a condition Anki move generator; from a b-p<thought-domain>that is the b.p-Anki # enter a sort() 
+style if-cond, even, $that-code, and render a Standard Anki Board Code<fbno-defd>which introduces the newly-E of that move, optionally, the E-diff in the res-sq, some, ini-sq, checkbxe, and some meta-analys<review-ki>
+The ini-End can be the $loc Simply then an array of endpoints, and total_M can be calc’d for that zi also, all, in 并行() We-dun: 造铁 must prec 使转 
+function 并行查找_op($line_名/*RorF🌭*/, $lim=16){ echo $line_名; }$R_王->并行查找_op(财产["地点"][0]); Uncaught Error: Using $this when not in object context
+Place the contents of class Rook_test and Bish_test from array.manipu Build the E_Sum pushing After, build $贴协 in zi_assembly_helper()<analy-1>Ultimately, what I need is E_SUM 
+<analy-1>$贴协✅<rails>⭐class-search⭐<E.Sum-Chu> 1) I did not yet build $lim, and 2) did not build E.Sum-Order Both are range-based, dep'n abs<val>Automate retri side information from class
+Bump line search into discrete traits Push to line.op-main and inc-Sep Output loop: array_length by count() both posteds; run a for by length loop and shut down the shorter; incr loop through the entire row # #unique 
+php.net/manual/en/array.sorting.php | assump-comp vs rank php.net/strcmp
+-->
+
+<style>.prot-E{background-color:00FF00}.neutr{background-color:607399;color:white}.eE{background-color:red}</style>
+<script>document.getElementsByTagName("TITLE")[0].text="target.order-test.php"</script>
+
